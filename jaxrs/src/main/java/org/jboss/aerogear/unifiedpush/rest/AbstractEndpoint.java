cc8c49aabd1b47ee451a867c0e5a08b2765a30df
Rafi Rainshtein <rafir@cb4.com>
2021-01-18 19:41:39 +0200

    CAPP-16565 Make CORS specific to CB4 URLs

--- jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/AbstractEndpoint.java
+++ jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/AbstractEndpoint.java
@@ -1,16 +1,53 @@
 package org.jboss.aerogear.unifiedpush.rest;
 
+import org.jboss.aerogear.unifiedpush.service.impl.spring.OAuth2Configuration;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AbstractEndpoint {
+    // Valid origin can only be "https://cb4/" or "https://*-mcs.c-b4.com/*"
+    // (Super-Pharm is an exception due to its existence in production)
+    private static List<Pattern> originPatterns =
+            convertRegexStringsToPatterns(
+                    Stream.concat(Arrays.asList("^https://cb4/",
+                                        "^https://(([a-zA-Z0-9-]+)(-|.))?mcs.c-b4.(com|info)/?([a-zA-Z0-9./]*)").stream(),
+                            OAuth2Configuration.getStaticCorsValidOrigins().stream())
+                            .collect(Collectors.toList()));
+
+    private static List<Pattern> convertRegexStringsToPatterns(List<String> regexStrings) {
+        List<Pattern> patterns = new ArrayList<Pattern>();
+        for(String rs: regexStrings) {
+            patterns.add(Pattern.compile(rs));
+        }
+        return patterns;
+    }
+
+    private static String returnIfValidOrigin(String originUrl) {
+        if(originUrl!=null && !originUrl.isEmpty()) {
+            for(Pattern pattern: originPatterns) {
+                if(pattern.matcher(originUrl).matches())
+                    return originUrl;
+            }
+        }
+        return null;  // An null origin should be dropped
+    }
+
 	protected static ResponseBuilder appendPreflightResponseHeaders(HttpHeaders headers, ResponseBuilder response) {
         // add response headers for the preflight request
         // required
-        response.header("Access-Control-Allow-Origin", headers.getRequestHeader("Origin").get(0)) // return submitted origin
+        String acaoUrl = returnIfValidOrigin(headers.getRequestHeader("Origin").get(0));
+        response.header("Access-Control-Allow-Origin", acaoUrl) // return submitted origin
+                .header("Vary",(acaoUrl!=null ? "Origin" : null))
                 .header("Access-Control-Allow-Methods", "POST, DELETE, PUT, GET, OPTIONS")
                 .header("Access-Control-Allow-Headers", "accept, origin, content-type, authorization, device-token") // explicit Headers!
                 .header("Access-Control-Allow-Credentials", "true")
@@ -21,8 +58,10 @@ public class AbstractEndpoint {
     }
 
 	protected Response appendAllowOriginHeader(ResponseBuilder rb, HttpServletRequest request) {
-		return rb.header("Access-Control-Allow-Origin", request.getHeader("Origin")) // return submitted origin
-				.header("Access-Control-Allow-Credentials", "true")
+        String acaoUrl = returnIfValidOrigin(request.getHeader("Origin"));
+        return rb.header("Access-Control-Allow-Origin", acaoUrl) // return submitted origin
+                .header("Vary",(acaoUrl!=null ? "Origin" : null))
+                .header("Access-Control-Allow-Credentials", "true")
 				.build();
     }
 
